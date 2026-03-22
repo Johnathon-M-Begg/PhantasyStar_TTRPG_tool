e219ba09cc10f6cf841b94c87d3c7f24802ac235
JonBegg <epic1996jboy@gmail.com>
2026-03-21 20:16:00 -0400

    Remove node_modules from tracking

---
+++ src/components/CreateCharacter.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './CreateCharacter.css'
+
+function CreateCharacter() {
+    const navigate = useNavigate()
+    const [selectedRace, setSelectedRace] = useState('')
+
+    const races = [
+        'Hume',
+        'Newmans',
+        'Casts',
+        'Beasts'
+    ]
+
+    return (
+        <div className="container">
+            <div className="white-box">
+                <h2>Create New Character</h2>
+                <div className="form-group">
+                    <label htmlFor="race-select">Select Race:</label>
+                    <select 
+                        id="race-select" 
+                        value={selectedRace}
+                        onChange={(e) => setSelectedRace(e.target.value)}
+                        className="dropdown"
+                    >
+                        <option value="">-- Choose a race --</option>
+                        {races.map((race) => (
+                            <option key={race} value={race}>
+                                {race}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="button-group">
+                    <button className="btn btn-secondary" onClick={() => navigate('/')}>Back</button>
+                    <button className="btn btn-primary" disabled={!selectedRace}>Next</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateCharacter
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import './RaceSelect.css'
+
+function RaceSelect() {
+    return (
+        <div className="container">
+            <div className="white-box">
+                <button className="btn btn-primary">CREATE NEW</button>
+                <button className="btn btn-secondary">LOAD</button>
+            </div>
+        </div>
+    )
+}
+
+export default RaceSelect
