bcdeb46b08cbd981a1165166e4d9b35e71fe49cd
JonBegg <epic1996jboy@gmail.com>
2026-04-05 01:30:23 -0400

    feature/1 adding VehicleType

---
+++ src/DataObjects/enums/VehicleType.tsx
@@ -0,0 +1,10 @@
+export enum VehicleType {
+    Bike = "Bike",
+    Car = "Car",
+    Hovercar = "Hovercar",
+    HugeBeast = "Huge Beast",
+    Hydrofoil = "Hydrofoil",
+    Landover = "Landover",
+    LargeBeast = "Large Beast",
+    Waterbike = "Waterbike",
+}
