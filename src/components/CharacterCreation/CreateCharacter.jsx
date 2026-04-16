2de306cb89d9722d90c29d032208c31e8bdf21bf
JonBegg <epic1996jboy@gmail.com>
2026-04-15 23:49:56 -0400

    feature/1 created character summary

--- src/components/CharacterCreation/CreateCharacter.jsx
+++ src/components/CharacterCreation/CreateCharacter.jsx
@@ -13,36 +13,36 @@ import {BackgroundEnum} from "../../DataObjects/enums/BackgroundEnum.tsx";
 import {ProfessionEnum} from "../../DataObjects/enums/ProfessionEnum.tsx";
 
 function CreateCharacter() {
-    const [selectedProfession, setSelectedProfession] = useState(ProfessionEnum.Guardian)
+    const [selectedProfession, setSelectedProfession] = useState(null)
     const [selectedOrigin, setSelectedOrigin] = useState({
-        race: RaceEnum.Human,
+        race: null,
         variant: VariantEnum.Default,
-        background: BackgroundEnum.Wayfarer,
+        background: null,
     })
     const [step, setStep] = useState(1)
     const [abilityScores, setAbilityScore] = useState({
-        Strength: 3,
-        Dexterity: -1,
-        Constitution: 2,
+        Strength: 0,
+        Dexterity: 0,
+        Constitution: 0,
         Intelligence: 0,
-        Wisdom: 2,
-        Charisma: 1,
+        Wisdom: 0,
+        Charisma: 0,
     })
     const [skillRanks, setSkillRanks] = useState({
         acrobatics: 0,
         astrophysics: 0,
-        athletics: 2,
+        athletics: 0,
         computers: 0,
         deception: 0,
-        insight: 3,
+        insight: 0,
         intimidation: 0,
         investigation: 0,
         lore: 0,
         mechanics: 0,
         medicine: 0,
         performance: 0,
-        perception: 3,
-        persuasion: 3,
+        perception: 0,
+        persuasion: 0,
         sleightOfHand: 0,
         stealth: 0,
         survival: 0,
@@ -61,7 +61,7 @@ function CreateCharacter() {
     return (
         <Box className="container">
             <Box className="white-box">
-                {step === 0 && (
+                {step === 1 && (
                     <OriginSelect
                         selectedOrigin={selectedOrigin}
                         setSelectedOrigin={setSelectedOrigin}
@@ -106,7 +106,7 @@ function CreateCharacter() {
                         stepBackward={stepBackward}
                     />
                 )}
-                {step === 1 && (
+                {step === 6 && (
                     <CharacterSummary
                         profession={selectedProfession}
                         race={selectedOrigin.race}
